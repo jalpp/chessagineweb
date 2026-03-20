4e2ef53cef7efac6ea6751666f01e662d060e2ab
jalpp <jalp.panchal44@gmail.com>
2026-03-19 21:31:48 -0400

    chore: fix nextConfig for stripe

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ["@mastra/*"],
-    productionBrowserSourceMaps: process.env.ENABLE_SOURCE_MAPS === 'true',
-    headers() {
-        const headers = [
-            {
-                source: '/:path*',
-                headers: ENGINE_HEADERS,
-            },
-            {
-                source: '/static/:path*',
-                headers: ENGINE_HEADERS.concat({
-                    key: 'Cache-Control',
-                    value: 'public, max-age=2592000, immutable',
-                }),
-            },
-        ];
-
-        return headers;
-    },
+  serverExternalPackages: ["@mastra/*"],
+  productionBrowserSourceMaps: process.env.ENABLE_SOURCE_MAPS === "true",
+  headers() {
+    return [
+     
+      {
+        source: "/(position|play|puzzle|game)/:path*",
+        headers: ENGINE_HEADERS,
+      },
+      {
+        source: "/static/:path*",
+        headers: ENGINE_HEADERS.concat({
+          key: "Cache-Control",
+          value: "public, max-age=2592000, immutable",
+        }),
+      },
+   
+      {
+        source: "/((?!position|play|puzzle|game|static).*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups", 
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const ENGINE_HEADERS = [
-    {
-        key: 'Cross-Origin-Embedder-Policy',
-        value: 'require-corp',
-    },
-    {
-        key: 'Cross-Origin-Opener-Policy',
-        value: 'same-origin',
-    },
+  {
+    key: "Cross-Origin-Embedder-Policy",
+    value: "require-corp",
+  },
+  {
+    key: "Cross-Origin-Opener-Policy",
+    value: "same-origin",
+  },
 ];
-
-
 
 export default nextConfig;
